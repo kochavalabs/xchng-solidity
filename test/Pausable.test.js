18e92219595becb9eb1576e88e8eff868fd17f17
jeff1010322 <jsimpson@kochava.com>
2018-05-16 14:26:54 -0400

    test(Pausable): added test file for Pausable contract

--- test/Pausable.test.js
+++ test/Pausable.test.js
@@ -34,9 +34,15 @@ contract('Pausable', async ([owner, other]) => {
         });
 
         it('should emit Pause event', async function () {
+<<<<<<< HEAD
             const { logs } =  await this.pausable.pause();
             assert.equal(logs.length, 1);
             assert.equal(logs[0].event, 'Pause');
+=======
+            await this.pausable.pause();
+            
+            // TODO: 
+>>>>>>> test(Pausable): added test file for Pausable contract
         });
     });
 
@@ -61,9 +67,15 @@ contract('Pausable', async ([owner, other]) => {
         });
 
         it('should emit Unpause event', async function () {
+<<<<<<< HEAD
             const { logs } = await this.pausable.unpause();
             assert.equal(logs.length, 1);
             assert.equal(logs[0].event, 'Unpause');
+=======
+            await this.pausable.unpause();
+            
+            // TODO: 
+>>>>>>> test(Pausable): added test file for Pausable contract
         });
     });
 });
