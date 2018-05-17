9c8c032fa9c01ecc9c8bf576c3c08be22b1d64eb
jeff1010322 <jsimpson@kochava.com>
2018-05-17 09:44:20 -0400

    test(Pausable): implemented test to check emitted events

--- test/Pausable.test.js
+++ test/Pausable.test.js
@@ -34,9 +34,9 @@ contract('Pausable', async ([owner, other]) => {
         });
 
         it('should emit Pause event', async function () {
-            await this.pausable.pause();
-            
-            // TODO: 
+            const { logs } =  await this.pausable.pause();
+            assert.equal(logs.length, 1);
+            assert.equal(logs[0].event, 'Pause');
         });
     });
 
@@ -61,9 +61,9 @@ contract('Pausable', async ([owner, other]) => {
         });
 
         it('should emit Unpause event', async function () {
-            await this.pausable.unpause();
-            
-            // TODO: 
+            const { logs } = await this.pausable.unpause();
+            assert.equal(logs.length, 1);
+            assert.equal(logs[0].event, 'Unpause');
         });
     });
 });
