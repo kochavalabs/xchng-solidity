d641decfd7d6e72b2985ad1a34da84a077d9bf92
jeff1010322 <jsimpson@kochava.com>
2018-05-21 16:41:24 -0400

    feat(Testnet): added ropsten network to the truffle network configs

--- truffle.js
+++ truffle.js
@@ -1,12 +1,24 @@
 require('babel-register');
 require('babel-polyfill');
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = process.env.MNEMONIC;
+const node = process.env.NODE_ADDR;
+
 module.exports = {
 networks: {
     ganache: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777" // matching any id
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, node)
+      },
+      network_id: 3,
+      gas: 4700000
     },
     coverage: {
       host: "127.0.0.1",
