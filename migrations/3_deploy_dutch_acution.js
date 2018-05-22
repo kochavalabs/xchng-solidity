3405f128b180397fd0fe32e61ad5029c76cac936
jeff1010322 <jeff1010322@yahoo.com>
2018-05-22 16:50:51 -0400

    refactor(Migration): fixed mixed case address for DutchAuction

--- migrations/3_deploy_dutch_acution.js
+++ migrations/3_deploy_dutch_acution.js
@@ -1,10 +1,10 @@
-var XchngToken = artifacts.require("./XchngToken.sol");
-var DutchAuction = artifacts.require("./DutchAuction.sol");
+var XchngToken = artifacts.require("./token/XchngToken.sol");
+var DutchAuction = artifacts.require("./crowdsale/DutchAuction.sol");
 
 module.exports = function (deployer) {
   deployer.link(XchngToken, DutchAuction);
   deployer.deploy(DutchAuction,
-    '0x8EF797325F65aa636027Bb13ae4aBcF32F5a7ec9', // Wallet used to collect funds
+    '0x8ef797325f65aa636027bb13ae4abcf32f5a7ec9', // Wallet used to collect funds
     2000000000000000000,  // Price start
     174640000, // Price Constant
     3  // Price Exponent
