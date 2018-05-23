9bb6859060fb4db7f84f2f46f3029c3526cf1a0d
jeff1010322 <jeff1010322@yahoo.com>
2018-05-23 16:22:11 -0400

    feat(scripts): added script to whitelist address on ropsten using hdwallet

--- scripts/hdwallet.js
+++ scripts/hdwallet.js
@@ -2,11 +2,14 @@ const Web3 = require('web3');
 const auction = require('../build/contracts/DutchAuction.json');
 const token = require('../build/contracts/XchngToken.json');
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const { awaitHandler } = require('./util');
+const { awaitHandler, STAGES } = require('./util');
 
 // Sets the hdwallet based on environment variables
 const mnemonic = process.env.MNEMONIC;
 const node = process.env.NODE_ADDR;
+
+// How many tokens to start with
+const NUM_TOKENS_AVAILABLE = process.env.NUM_TOKENS_AVAILABLE || '600000000000000000000000000';
 
 hdwallet = new HDWalletProvider(mnemonic, node);
 
@@ -33,6 +36,48 @@ const getAccounts = async () => {
     return accounts;
 };
 
+const getStage = async () => {
+    let stage;
+    let err;
+  
+    // Get the current auction stage
+    [stage, err] = await awaitHandler(dutchAuction.methods.stage().call());
+    if (err != null) {
+      console.log('Error getting auction stage: ', err);
+      process.exit(1);
+    }
+  
+    switch (stage) {
+      case '0':
+        return STAGES.AuctionDeployed;
+      case '1':
+        return STAGES.Setup;
+      case '2':
+        return STAGES.AuctionStarted;
+      case '3':
+        return STAGES.AuctionEnded;
+      case '4':
+        return STAGES.TokensDistributed;
+      default:
+        // It should be impossible to get here, but just in-case
+        return { enum: '-1', name: 'Unknown' };
+    }
+};
+
+const whitelist = async (address, owner) => {
+    let _;
+    let err;
+  
+    [_, err] = await awaitHandler(dutchAuction.methods.addAddressToWhitelist(address).send({ from: owner, gas: 1000000 }));
+    if (err != null) {
+      console.log('Error whitelisting address: ', err);
+      process.exit(1);
+    }
+  
+    return true;
+};
+  
+
 module.exports = {
-    getAccounts, dutchAuction, xchngToken,
+    dutchAuction, xchngToken, NUM_TOKENS_AVAILABLE, AUCTION_ADDRESS, TOKEN_ADDRESS, getAccounts, getStage, whitelist,
 }
---
+++ scripts/whitelist.js
@@ -0,0 +1,22 @@
+/* eslint no-unused-vars: "off", prefer-const: "off" */
+const {
+  getAccounts, whitelist,
+} = require('./hdwallet');
+
+async function addToWhitelist(address) {
+    const accounts = await getAccounts();
+
+    console.log('Whitelisting address', myArgs[0]);
+    await whitelist(myArgs[0], accounts[0]);
+
+    console.log('Account Successfully whitelisted');
+}
+
+var myArgs = process.argv.slice(2);
+
+if (myArgs.length != 1) {
+  console.log('Usage: node whitelist.js [Address]');
+  process.exit(1);
+}
+
+addToWhitelist(myArgs[0])
